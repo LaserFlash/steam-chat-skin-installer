42771c02f006e298f0ad3b8e41d24362cb95aca6
Bryn Bennett <bryn.t.bennett@gmail.com>
2019-08-17 21:37:52 +1200

    Features: - create from pasted texr - install local file - general script tidy up

--- FriendsSkinInstaller.bat
+++ FriendsSkinInstaller.bat
@@ -31,32 +31,40 @@ echo.
 
 :start
 echo Choose an option:
-echo 1. Download the latest theme version.
-echo 2. Copy friends.custom.css to Steam Directory.
-echo 3. Open friends.custom.css for customisation
-echo 4. Exit
+echo 1. Install theme from already downloaded file.
+echo 2. Create friends.custom.css from clipboard.
+echo 3. Download LaserFlash's default theme.
+echo 4. Copy friends.custom.css to Steam Directory.
+echo 5. Open friends.custom.css for customisation
+echo 6. Exit
 echo.
 set /p choice= Your choice: 
-if %choice%==1 call:update goto:start
-if %choice%==2 call:copy goto:start
-if %choice%==3 call:edit goto:start
-if %choice%==4 goto:eof
-echo Invalid selection, please try again.
-goto:start
+if %choice%==1 call:copy
+if %choice%==2 call:clipboardCreation
+if %choice%==3 call:download
+if %choice%==4 call:copy
+if %choice%==5 call:edit
+if %choice%==6 goto:eof
+if %choice% gtr 6 ( echo Invalid selection, please try again. && goto:start )
+if %choice% leq 0 ( echo Invalid selection, please try again. && goto:start )
 
+:clipboardCreation
+    set /p cssImports= Paste friends.custom.css here: 
+    echo writing to file
+    echo %cssImports% > friends.custom.css
+    goto:done
 
-:update
+:download
     echo.
     echo Dowloading Files to current folder... 
-        powershell -Command "Try{[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12;(New-Object Net.WebClient).DownloadFile('https://raw.githubusercontent.com/LaserFlash/steam-chat-skin/master/src/friends.custom.css', 'friends.custom.css')}Catch{Write-Warning $($error[0]);pause}"
-
+    powershell -Command "Try{[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12;(New-Object Net.WebClient).DownloadFile('https://raw.githubusercontent.com/LaserFlash/steam-chat-skin/master/src/friends.custom.css', 'friends.custom.css')}Catch{Write-Warning $($error[0]);pause}"
     echo all files have been downloaded succesfully.
     echo.
-goto:eof
+    goto:done
 
-    :copy
+:copy
     echo.
-    if not exist friends.custom.css (echo  friends.custom.css not found, make sure it's in the same directory as this installer. && echo. && goto:start)
+    if not exist friends.custom.css ( echo  friends.custom.css not found, make sure it's in the same directory as this installer. && echo. && goto:start )
 
     echo Checking for Steam directory...
     for /f "tokens=1,2*" %%E in ('reg query HKEY_CURRENT_USER\Software\Valve\Steam\') do (
@@ -80,10 +88,15 @@ goto:eof
     )
     if not [%FriendsSkin%]==[] copy /Y friends.custom.css "%FriendsPath%" >nul
     echo.
-goto:eof
+    goto:done
 
 :edit
-    if not exist friends.custom.css (echo  friends.custom.css not found, Downloading it now. && echo. && Call:update)
+    if not exist friends.custom.css (echo  friends.custom.css not found, Downloading default now. && echo. && call:download)
     echo Opening default text editor
     start "" friends.custom.css
-goto:eof
+    goto:done
+
+:done   
+    echo Operation completed
+    echo If you want to perform another task please select one 
+    goto:start
